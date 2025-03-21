12d1582deb17bc98e0892a707ba1a7f2e7b4466f
Firemuu <106168942+Firemuu@users.noreply.github.com>
2025-03-21 19:50:50 +0100

    Delete CY_WEBSITE-main directory

--- CY_WEBSITE-main/src/news/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request,'news/index.html')
-
-def article(request, number_article):
-    #Ajouter la condition qui verifie si le numero de l'article est dispo 
-    return render(request,f'news/article{number_article}')
