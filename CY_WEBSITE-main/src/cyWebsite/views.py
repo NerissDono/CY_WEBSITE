9ec4f11f21c249281547af64e5411276568f651d
Firemuu <106168942+Firemuu@users.noreply.github.com>
2025-03-22 09:35:13 +0100

    Delete CY_WEBSITE-main directory

--- CY_WEBSITE-main/src/cyWebsite/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request,'cyWebsite/index.html')
-
-def login(request):
-    return render(request,'cyWebsite/login.html')
-
