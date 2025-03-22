f94b6940a683dea002c03a9e1be3b6138e850364
davekg8 <davidegbakou@gmail.com>
2025-03-22 16:48:52 +0100

    ajout des enregistrements d'administration pour les modèles Article, Author, Category et Comment

--- src/news/admin.py
+++ src/news/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 from .models import Article, Author, Category, Comment
 
-admin.site.register(Article)
-admin.site.register(Author)
-admin.site.register(Category)
-admin.site.register(Comment)
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date')
+    list_filter = ('published_date', 'author')
+    search_fields = ('title', 'content')
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email')
+    search_fields = ('name', 'email')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_date')
+    list_filter = ('created_date',)
+    search_fields = ('name', 'email')
+
