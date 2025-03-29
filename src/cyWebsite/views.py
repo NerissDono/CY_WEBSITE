14af6edb1638f45134b5c94187a98cabedd5625d
Firemuu <106168942+Firemuu@users.noreply.github.com>
2025-03-29 11:01:14 +0100

    Add files via upload

--- src/cyWebsite/views.py
+++ src/cyWebsite/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from news.models import Article
 
 def index(request):
-    return render(request, 'cyWebsite/index.html')
+    articles = Article.objects.select_related('author').all()  # Fetch articles with their authors
+    return render(request, 'cyWebsite/index.html', {'articles': articles})
 
 
 
