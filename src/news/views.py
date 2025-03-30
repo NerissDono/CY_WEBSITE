5ea576bcad1eac6f479dd55c341f557f911c05be
Firemuu <106168942+Firemuu@users.noreply.github.com>
2025-03-30 08:45:51 +0200

    Add files via upload

--- src/news/views.py
+++ src/news/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required, user_passes_test
+from user.decorators import xp_level_required
 
 def accueil(request):
     if not request.user.is_authenticated:
@@ -14,47 +15,18 @@ def information(request):
     return render(request, 'news/information.html')
 
 @login_required
+@xp_level_required('complex')
 def gestion(request):
     return render(request, 'news/gestion.html')
 
 @user_passes_test(lambda u: u.is_superuser)
+@xp_level_required('admin')
 def administration(request):
     return render(request, 'news/administration.html')
 
+@login_required
 def visualisation(request):
-    vehicules = [
-        {
-            "nom": "Peugeot 403 Rouge",
-            "plaque": "AB-123-CD",
-            "telephone": "06 11 22 33 44",
-            "image": "vehicules/voiture1.png"
-        },
-        {
-            "nom": "BMW M3 Grise",
-            "plaque": "EF-456-GH",
-            "telephone": "07 55 66 77 88",
-            "image": "vehicules/voiture2.png"
-        },
-        {
-            "nom": "Mercedes GLA Blanche",
-            "plaque": "IJ-789-KL",
-            "telephone": "06 98 76 54 32",
-            "image": "vehicules/voiture3.png"
-        },
-        {
-            "nom": "Mercedes GLE Grise",
-            "plaque": "MN-321-OP",
-            "telephone": "07 33 22 11 00",
-            "image": "vehicules/voiture4.png"
-        },
-        {
-            "nom": "Dacia Duster Blanche",
-            "plaque": "QR-654-ST",
-            "telephone": "06 70 80 90 10",
-            "image": "vehicules/voiture5.png"
-        },
-    ]
-    return render(request, "news/visualisation.html", {"vehicules": vehicules})
+    return render(request, "news/visualisation.html", {"user": request.user})
 
 def register(request):
     if request.method == 'POST':
